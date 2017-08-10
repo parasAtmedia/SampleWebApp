564d0b0cef547641908ace53b8b9d588899d5b82
paras.a <paras.a@media.net>
2017-08-10 13:11:31 +0530

    HTML id conventions redefined, for ease of work between 2 team mates

--- demo.js
+++ demo.js
@@ -1,22 +1,33 @@
 $(document).ready(function () {
-    $("#opt1").click(function () {
-        $("#page1").toggle();
-        $("#page2").hide();
-        $("#page3").hide();
+    $("#a_nav1").click(function () {
+        $("#sections_nav1").toggle();
+        $("#sections_nav2").hide();
+        $("#sections_nav3").hide();
+        $("#sections_nav4").hide();
         console.log("page1click");
     });
 
-    $("#opt2").click(function () {
-        $("#page2").toggle();
-        $("#page1").hide();
-        $("#page3").hide();
+    $("#a_nav2").click(function () {
+        $("#sections_nav2").toggle();
+        $("#sections_nav1").hide();
+        $("#sections_nav3").hide();
+        $("#sections_nav4").hide();
         console.log("page2click");
     });
 
-    $("#opt3").click(function () {
-        $("#page3").toggle();
-        $("#page1").hide();
-        $("#page2").hide();
+    $("#a_nav3").click(function () {
+        $("#sections_nav3").toggle();
+        $("#sections_nav2").hide();
+        $("#sections_nav1").hide();
+        $("#sections_nav4").hide();
         console.log("page3click");
+    });
+
+    $("#a_nav4").click(function () {
+        $("#sections_nav4").toggle();
+        $("#sections_nav2").hide();
+        $("#sections_nav1").hide();
+        $("#sections_nav3").hide();
+        console.log("page4click");
     });
 });
